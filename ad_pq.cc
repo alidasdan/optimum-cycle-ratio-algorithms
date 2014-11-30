c7102c8f52fd5f24fd683a0fe0eddedaaef676c1
Ali Dasdan <dasdan@dreamoid.com>
2014-11-29 19:16:45 -0800

    initial commit to github

---
+++ ad_pq.cc
@@ -0,0 +1,11 @@
+//
+// COPYRIGHT C 1999- Ali Dasdan (ali_dasdan@yahoo.com)
+//
+//#include "new.h"
+#include <new>
+#include "ad_pq.h"
+
+// Instantiate instances of ad_pq with types.
+template class ad_pq< int, int >;
+template class ad_pq< float, int >;
+// End of file
